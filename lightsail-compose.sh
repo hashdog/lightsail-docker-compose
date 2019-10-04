a57d94cb5e6195e6100533ddb4ed20fae079a077
COLCO <mario@hashdog.com>
2019-10-04 19:38:52 -0300

    update script

--- lightsail-compose.sh
+++ lightsail-compose.sh
@@ -7,7 +7,7 @@ curl -sSL https://get.docker.com | sh
 usermod -aG docker ubuntu
 
 # install docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 # copy the dockerfile into /srv/docker
