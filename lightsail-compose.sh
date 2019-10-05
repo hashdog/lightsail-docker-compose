b1e9a327d1338dae7c8cd041466a12ee6d513378
COLCO <mario@hashdog.com>
2019-10-04 21:03:36 -0300

    remove apt update

--- lightsail-compose.sh
+++ lightsail-compose.sh
@@ -3,7 +3,6 @@
 # # install latest version of docker the lazy way
 # curl -sSL https://get.docker.com | sh
 apt update
-apt upgrade -y
 apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
