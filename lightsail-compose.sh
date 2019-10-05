d56d871ab0aab0da8b97338e0bb47268c11fe968
COLCO <mario@hashdog.com>
2019-10-04 21:26:03 -0300

    update script

--- lightsail-compose.sh
+++ lightsail-compose.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-# # install latest version of docker the lazy way
-# curl -sSL https://get.docker.com | sh
-apt update
-apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-apt update
-apt-cache policy docker-ce
-apt install docker-ce
+# fix interactive install
+export DEBIAN_FRONTEND=noninteractive
+
+# install latest version of docker the lazy way
+curl -sSL https://get.docker.com | sh
 
 # make it so you don't need to sudo to run docker commands
 usermod -aG docker ubuntu
