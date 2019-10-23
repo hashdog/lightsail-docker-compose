d7905ce98dc78f629c90274d4d7b652b9fe368ae
COLCO <mario@hashdog.com>
2019-10-23 12:15:32 -0300

    update user setup script

--- lightsail-user-setup.sh
+++ lightsail-user-setup.sh
@@ -4,7 +4,7 @@
 mkdir ~/.npm-global
 npm config set prefix '~/.npm-global'
 
-echo "export PATH=~/.npm-global/bin:$PATH" >> ~/.profile
+echo 'export PATH=~/.npm-global/bin:$PATH' >> ~/.profile
 source ~/.profile
 
 # Config ssh
