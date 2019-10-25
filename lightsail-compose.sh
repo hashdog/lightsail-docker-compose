7ee5e798360b779da5e76f7da1b86e0752a76b2e
COLCO <mario@hashdog.com>
2019-10-25 17:19:17 -0300

    update script

--- lightsail-compose.sh
+++ lightsail-compose.sh
@@ -13,17 +13,5 @@ usermod -aG docker ubuntu
 curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-# copy the dockerfile into /srv/docker
-# if you change this, change the systemd service file to match
-# WorkingDirectory=[whatever you have below]
 mkdir -p /srv
-# mkdir -p /srv/docker
-# curl -o /srv/docker/docker-compose.yml https://raw.githubusercontent.com/hashdog/lightsail-docker-compose/master/docker-compose.yml
-
-# # copy in systemd unit file and register it so our compose file runs
-# # on system restart
-# curl -o /etc/systemd/system/docker-compose-app.service https://raw.githubusercontent.com/hashdog/lightsail-docker-compose/master/docker-compose-app.service
-# systemctl enable docker-compose-app
-
-# # start up the application via docker-compose
-# docker-compose -f /srv/docker/docker-compose.yml up -d
+chown ubuntu -R /srv
